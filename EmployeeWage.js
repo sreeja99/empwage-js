1f0b2d55537108588cff136a4e46e15b4ed533a0
sreeja99 <srijagodishala@gmail.com>
2020-11-07 19:01:28 +0530

    Added:Ability To Calculate Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,8 +1,18 @@
-const IS_ABSENT=0;
-let empCheck = Math.floor(Math.random()*10)%2;
-if(empCheck==0){
-    console.log("Employee is absent")
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HRS=4;
+const FULL_TIME_HRS=8;
+const WAGE_PER_HR=20;
+let empHrs = 0;
+let empCheck = Math.floor(Math.random()*10)%2 +1;
+console.log(empCheck);
+switch(empCheck){
+    case IS_PART_TIME:
+        empHrs=PART_TIME_HRS;
+        break;
+    case IS_FULL_TIME:
+        empHrs=FULL_TIME_HRS;
+        break;
 }
-else{
-    console.log("Employee is present")
-}
+let empWage=WAGE_PER_HR*empHrs;
+console.log("Total Employee Wage:"+empWage);
