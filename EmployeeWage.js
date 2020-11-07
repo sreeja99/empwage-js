27343c11a4ae44bbaf0ab7de81f7184503ed80a7
sreeja99 <srijagodishala@gmail.com>
2020-11-07 18:51:40 +0530

    Added:Condition To Check Whether Employee Is Present Or Absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,8 @@
-console.log("Welcome");
+const IS_ABSENT=0;
+let empCheck = Math.floor(Math.random()*10)%2;
+if(empCheck==0){
+    console.log("Employee is absent")
+}
+else{
+    console.log("Employee is present")
+}
