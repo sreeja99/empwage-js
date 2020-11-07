576ba0ab043ab536dae556920c2d12752d28e55a
sreeja99 <srijagodishala@gmail.com>
2020-11-07 19:08:56 +0530

    Refactored:Calculating Employee Wage Through Functions

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,16 +3,20 @@ const IS_FULL_TIME=2;
 const PART_TIME_HRS=4;
 const FULL_TIME_HRS=8;
 const WAGE_PER_HR=20;
-let empHrs = 0;
-let empCheck = Math.floor(Math.random()*10)%2 +1;
-console.log(empCheck);
+function getWorkingHrs(empCheck){
 switch(empCheck){
     case IS_PART_TIME:
-        empHrs=PART_TIME_HRS;
-        break;
+        console.log("Employee is part time");
+        return PART_TIME_HRS;
     case IS_FULL_TIME:
-        empHrs=FULL_TIME_HRS;
-        break;
+        console.log("Employee is full time");
+        return FULL_TIME_HRS;
+    default:
+        console.log("employee is absent");
+        return 0;
 }
+}
+let empCheck = Math.floor(Math.random()*10)%3;
+let empHrs=getWorkingHrs(empCheck)
 let empWage=WAGE_PER_HR*empHrs;
 console.log("Total Employee Wage:"+empWage);
