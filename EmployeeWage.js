c4a7da29f3f460807e0daf48e6becd80f5cb41cd
sreeja99 <srijagodishala@gmail.com>
2020-11-10 15:33:53 +0530

    Added:Maps And Arrows Are Used To Calculate Wages

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -114,3 +114,26 @@ function totalDaysEmployeeWorked(noOfDays,dailyWage){
 console.log("UC 7G- Number of days employee worked: "+empDailyWageArray.reduce(totalDaysEmployeeWorked,0));
 //8 Map Functions
 console.log("UC 8 Employee Wage Map Total Hrs:"+Array.from(empDailyWageMap.values()).reduce(totalWages,0));
+//UC9 Arrow functions
+const findTotal = (totalVal,dailyVal)=>{
+    return totalVal+dailyVal;
+}
+let count =0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
+let totalSalary = empDailyWageArray.filter(dailyWage=>dailyWage>0)
+                                    .reduce(findTotal,0);
+console.log("UC 9A : Wage with arrow => function: "+totalSalary);
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value,key)=>{
+    if(value==8) 
+        fullWorkingDays.push(key);
+    else if(value==4)
+        partWorkingDays.push(key);
+    else 
+        nonWorkingDays.push(key);
+});
+console.log("Full working days: "+fullWorkingDays);
+console.log("Part working days: "+partWorkingDays);
+console.log("Non working days: "+nonWorkingDays);
